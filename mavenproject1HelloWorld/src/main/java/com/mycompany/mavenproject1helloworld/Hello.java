6b02a9b53f3c1feea759c22d0195c3f8763d22a7
arseniy elefer <arseniy.elefer@gmail.com>
2022-10-31 01:27:39 +0300

    demo commit for my 1st prgrm

---
+++ mavenproject1HelloWorld/src/main/java/com/mycompany/mavenproject1helloworld/Hello.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1helloworld;
+
+/**
+ *
+ * @author Lenovo x270
+ */
+public class Hello {
+    public static void main (String args [])
+    {
+        System.out.println("Hello, World. I'm here again!");
+    }
+    
+}
