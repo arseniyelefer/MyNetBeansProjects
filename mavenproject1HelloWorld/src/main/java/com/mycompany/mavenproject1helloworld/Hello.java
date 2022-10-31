3c412e9688053dd4336afa10e410137a9cf54657
Arseniy <117011168+arseniyelefer@users.noreply.github.com>
2022-10-31 23:41:37 +0300

    my first demo commit "Hola World!"

--- mavenproject1HelloWorld/src/main/java/com/mycompany/mavenproject1helloworld/Hello.java
+++ mavenproject1HelloWorld/src/main/java/com/mycompany/mavenproject1helloworld/Hello.java
@@ -12,6 +12,13 @@ public class Hello {
     public static void main (String args [])
     {
         System.out.println("Hello, World. I'm here again!");
+        System.out.println("Hello, World. I'm here again!");
+        System.out.println("Hello, World. I'm here again!");
+        System.out.println("Hello, World. I'm here again!");
+        System.out.println("Hello, World. I'm here again!");
+        System.out.println("Hello, World. I'm here again!");
+        System.out.println("Hello, World. I'm here again!");
+        
     }
     
 }
