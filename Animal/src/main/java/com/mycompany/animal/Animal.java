5c824cdceca928cbcb82b70634e7e7ef08de6dd7
Arseniy <arseniy.elefer@gmail.com>
2022-11-13 21:04:45 +0300

    class inheritance (Animal-Dog and Cat)

---
+++ Animal/src/main/java/com/mycompany/animal/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animal;
+
+/**
+ *
+ * @author Lenovo x270
+ */
+public class Animal {
+    public void eat () {
+        System.out.println("Im eating");
+    }
+    public void sleep () {
+        System.out.println("Im sleeping");
+    }
+}
---
+++ Animal/src/main/java/com/mycompany/animal/Lesson_Class_Inheritance.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animal;
+
+/**
+ *
+ * @author Lenovo x270
+ */
+public class Lesson_Class_Inheritance {
+    public static void main (String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.sleep();
+        
+    }
+}
